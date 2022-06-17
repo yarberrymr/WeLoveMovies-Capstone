71ecc94ef8d2a043ef51565c72f2b14d6739728e
Michael Yarberry <101155448+yarberrymr@users.noreply.github.com>
2022-06-17 10:07:52 -0500

    Added code comments

--- src/theaters/theaters.service.js
+++ src/theaters/theaters.service.js
@@ -1,9 +1,11 @@
 const db = require("../db/connection");
 
+//returns all theaters from theaters table for use in the controller
 function list() {
   return db("theaters");
 }
 
+//returns theaters paired with movies for use in the controller
 function getMovies(theaterId) {
   return db("theaters as t")
     .join("movies_theaters as mt", "t.theater_id", "mt.theater_id")
@@ -12,6 +14,7 @@ function getMovies(theaterId) {
     .where({ "t.theater_id": theaterId });
 }
 
+//module exports
 module.exports = {
   list,
   getMovies,
